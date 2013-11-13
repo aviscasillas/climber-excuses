76e16b2fb0cf8e854b9e2e9832dedaf9a4a8f888
Albert Viscasillas <aviscasillas@gmail.com>
2013-11-13 11:02:33 +0100

    Seeds added
     - Refactor seeds to avoid remove all the seeds every time that they
       run.
     - New excuses added.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,448 @@
-Excuse.all.each do |e|
-  e.remove
-end
-Excuse.create(msg: { 
-  en: 'Today is too wet',
-  es: 'Hoy hace mucha humedad'
-})
-Excuse.create(msg: {
-  en: 'This rock is too soft',
-  es: 'Esta roca se desace mucho'
-})
-Excuse.create(msg: {
-  en: 'If I would be higher...',
-  es: 'Si fuera un poco mas alto...' 
-})
+Excuse.new(_id: 1, msg: { 
+  en: "Wrong shoes",
+  es: "Estos pies de gato no me van bien"
+}).upsert
+Excuse.new(_id: 2, msg: { 
+  en: "The sun was in my eyes",
+  es: "Me da el sol en los ojos"
+}).upsert
+Excuse.new(_id: 3, msg: { 
+  en: "The holds are too far away",
+  es: "Ese canto esta muy lejos"
+}).upsert
+Excuse.new(_id: 2, msg: { 
+  en: "The rock was too hot my hands was sweaty",
+  es: "La roca esta muy caliente y me sudan las manos"
+}).upsert
+Excuse.new(_id: 4, msg: { 
+  en: "Hangover",
+  es: "---"
+}).upsert
+Excuse.new(_id: 5, msg: { 
+  en: "I'm too pumped",
+  es: "---"
+}).upsert
+Excuse.new(_id: 6, msg: { 
+  en: "I think that I cut my unas too short",
+  es: "Creo que me he cortado demasiado las unas"
+}).upsert
+Excuse.new(_id: 7, msg: { 
+  en: "I smoked too much",
+  es: "He fumado demasiado"
+}).upsert
+Excuse.new(_id: 8, msg: { 
+  en: "I ate a big breakfast",
+  es: "He desayunado mucho"
+}).upsert
+Excuse.new(_id: 9, msg: { 
+  en: "I'm too short",
+  es: "Soy demasiado bajo"
+}).upsert
+Excuse.new(_id: 10, msg: { 
+  en: "I don't like this kind of rock",
+  es: "No me gusta este tipo de roca"
+}).upsert
+Excuse.new(_id: 11, msg: { 
+  en: "I swear I could do it yesterday",
+  es: "Creo que lo podria haber hecho ayer"
+}).upsert
+Excuse.new(_id: 12, msg: { 
+  en: "I didn't trust the belayer enough, so i didn't try my best",
+  es: "---"
+}).upsert
+Excuse.new(_id: 13, msg: { 
+  en: "I can't do this with these pants",
+  es: "No puedo hecer-lo con estos pantalones"
+}).upsert
+Excuse.new(_id: 14, msg: { 
+  en: "---",
+  es: "De esa chapa no fio"
+}).upsert
+Excuse.new(_id: 15, msg: { 
+  en: "Holds are getting too polished from all the climbers",
+  es: "Los cantos estan muy sobaos"
+}).upsert
+Excuse.new(_id: 16, msg: { 
+  en: "I'm too tall",
+  es: "Soy demasiado alto"
+}).upsert
+Excuse.new(_id: 17, msg: { 
+  en: "My rope is too heavy",
+  es: "Mi cuerda es demasiado rigida y me cuesta chapar"
+}).upsert
+Excuse.new(_id: 18, msg: { 
+  en: "I forgot to clip my nails",
+  es: "---"
+}).upsert
+Excuse.new(_id: 19, msg: { 
+  en: "I'm a great boulderer, but I suck on routs",
+  es: "---"
+}).upsert
+Excuse.new(_id: 20, msg: { 
+  en: "My harness is too tight",
+  es: "---"
+}).upsert
+Excuse.new(_id: 21, msg: { 
+  en: "I really prefer trad...",
+  es: "---"
+}).upsert
+Excuse.new(_id: 22, msg: { 
+  en: "Rope drag is a bitch!",
+  es: "Arrastrar la cuerda es una mierda!"
+}).upsert
+Excuse.new(_id: 23, msg: { 
+  en: "Look at those kids! I'm just too old...",
+  es: "Mira estos crios! Yo ya soy muy mayor..."
+}).upsert
+Excuse.new(_id: 24, msg: { 
+  en: "I really can't concentrate when people watch me climb",
+  es: "Realmente no me puedo concentrar cuando la gente me mira"
+}).upsert
+Excuse.new(_id: 25, msg: { 
+  en: "This is my old rope, and I don't want to take falls on it",
+  es: "Esta es mi cuerda vieja, no mola hacer saques con ella"
+}).upsert
+Excuse.new(_id: 26, msg: { 
+  en: "This is my new rope, and I don't want to take falls on it",
+  es: "Esta es mi cuerda nueva, y no la quiero gastar haciendo saques"
+}).upsert
+Excuse.new(_id: 27, msg: { 
+  en: "I forgot to eat today",
+  es: "Hoy he olvidado comer"
+}).upsert
+Excuse.new(_id: 28, msg: { 
+  en: "I'm dehydrated",
+  es: "Estoy deshidratado"
+}).upsert
+Excuse.new(_id: 29, msg: { 
+  en: "My hands are too big for this hold!",
+  es: "Tengo las manos demasiado grandes para este canto"
+}).upsert
+Excuse.new(_id: 30, msg: { 
+  en: "The holds are to slippery",
+  es: "---"
+}).upsert
+Excuse.new(_id: 31, msg: { 
+  en: "I had the flu last week... so I'm recovering",
+  es: "---"
+}).upsert
+Excuse.new(_id: 33, msg: { 
+  en: "I can't jump!",
+  es: "Es que asi no puedo saltar!"
+}).upsert
+Excuse.new(_id: 34, msg: { 
+  en: "I'm not wearing my climbing pants",
+  es: "No llevo puestos mis pantalones de escalada"
+}).upsert
+Excuse.new(_id: 35, msg: { 
+  en: "It's too cold and I can't feel the rock",
+  es: "Es que hace mucho frio y no siento la roca"
+}).upsert
+Excuse.new(_id: 36, msg: { 
+  en: "It's too late... for sure, tomorrow I will do it!!!",
+  es: "Se me hace tarde... manana encadenos seguro!!!"
+}).upsert
+Excuse.new(_id: 37, msg: { 
+  en: "---"
+  es: "Estoy fuera de mi zona de confort"
+}).upsert
+Excuse.new(_id: 38, msg: { 
+  en: "---"
+  es: "Ahí aleja"
+}).upsert
+Excuse.new(_id: 39, msg: { 
+  en: "---"
+  es: "Yo hubiera equipado mejor"
+}).upsert
+Excuse.new(_id: 40, msg: { 
+  en: "---"
+  es: "Los cantos grandes me petan"
+}).upsert
+Excuse.new(_id: 41, msg: { 
+  en: "---"
+  es: "Que me despeino"
+}).upsert
+Excuse.new(_id: 42, msg: { 
+  en: "---"
+  es: "No hay tacto"
+}).upsert
+Excuse.new(_id: 43, msg: { 
+  en: "---"
+  es: "Es que es ya la segunda vía"
+}).upsert
+Excuse.new(_id: 44, msg: { 
+  en: "---"
+  es: "Yo escalo bien de mañana"
+}).upsert
+Excuse.new(_id: 45, msg: { 
+  en: "---"
+  es: "Voy mejor a última hora"
+}).upsert
+Excuse.new(_id: 46, msg: { 
+  en: "---"
+  es: "Desayune mal"
+}).upsert
+Excuse.new(_id: 47, msg: { 
+  en: "---"
+  es: "Mi ocho no me daba confianza"
+}).upsert
+Excuse.new(_id: 48, msg: { 
+  en: "---"
+  es: "Es que se cayo el de la vía de al lado"
+}).upsert
+Excuse.new(_id: 49, msg: { 
+  en: "---"
+  es: "Lo mio  no es la técnica de pies"
+}).upsert
+Excuse.new(_id: 50, msg: { 
+  en: "---"
+  es: "Necesito un recauchute"
+}).upsert
+Excuse.new(_id: 51, msg: { 
+  en: "---"
+  es: "Es que son gatos nuevos"
+}).upsert
+Excuse.new(_id: 52, msg: { 
+  en: "---"
+  es: "Voy muy tenso"
+}).upsert
+Excuse.new(_id: 53, msg: { 
+  en: "---"
+  es: "Me llevas muy flojo"
+}).upsert
+Excuse.new(_id: 54, msg: { 
+  en: "---"
+  es: "Había una araña"
+}).upsert
+Excuse.new(_id: 55, msg: { 
+  en: "---"
+  es: "Padezco de sobreentrenamiento"
+}).upsert
+Excuse.new(_id: 56, msg: { 
+  en: "---"
+  es: "Dormí mal"
+}).upsert
+Excuse.new(_id: 57, msg: { 
+  en: "---"
+  es: "Desde que tengo hijos..."
+}).upsert
+Excuse.new(_id: 58, msg: { 
+  en: "---"
+  es: "No me puedo ir cansado"
+}).upsert
+Excuse.new(_id: 59, msg: { 
+  en: "---"
+  es: "Yo es que voy a escalar mañana"
+}).upsert
+Excuse.new(_id: 60, msg: { 
+  en: "---"
+  es: "Solo quería ver la secuencia"
+}).upsert
+Excuse.new(_id: 61, msg: { 
+  en: "---"
+  es: "Yo no venía a encadenar"
+}).upsert
+Excuse.new(_id: 62, msg: { 
+  en: "---"
+  es: "Ya empece petado"
+}).upsert
+Excuse.new(_id: 63, msg: { 
+  en: "---"
+  es: "No visualice antes de empezar"
+}).upsert
+Excuse.new(_id: 64, msg: { 
+  en: "---"
+  es: "Yo necesito dormir la siesta"
+}).upsert
+Excuse.new(_id: 65, msg: { 
+  en: "---"
+  es: "Paso que mañana toca currar"
+}).upsert
+Excuse.new(_id: 66, msg: { 
+  en: "---"
+  es: "Cuando viene menganito escalo más fluido"
+}).upsert
+Excuse.new(_id: 67, msg: { 
+  en: "---"
+  es: "Esta no es mi escuela"
+}).upsert
+Excuse.new(_id: 60, msg: { 
+  en: "---"
+  es: "No veía la linea"
+}).upsert
+Excuse.new(_id: 68, msg: { 
+  en: "---"
+  es: "Poner las cintas se me da mal"
+}).upsert
+Excuse.new(_id: 69, msg: { 
+  en: "---"
+  es: "Siempre ne caigo con la reunión en la cara"
+}).upsert
+Excuse.new(_id: 70, msg: { 
+  en: "---"
+  es: "Yo vine a volar"
+}).upsert
+Excuse.new(_id: 71, msg: { 
+  en: "---"
+  es: "Es que es de flexy"
+}).upsert
+Excuse.new(_id: 72, msg: { 
+  en: "---"
+  es: "Ahora es que no hago nada de campus"
+}).upsert
+Excuse.new(_id: 73, msg: { 
+  en: "---"
+  es: "Si es que yo no entreno"
+}).upsert
+Excuse.new(_id: 74, msg: { 
+  en: "---"
+  es: "Llevo unos meses de mala racha"
+}).upsert
+Excuse.new(_id: 75, msg: { 
+  en: "---"
+  es: "Desde que vendí la furgoneta"
+}).upsert
+Excuse.new(_id: 76, msg: { 
+  en: "---"
+  es: "Bájame necesito beber"
+}).upsert
+Excuse.new(_id: 77, msg: { 
+  en: "---"
+  es: "Bájame que no lo veo"
+}).upsert
+Excuse.new(_id: 78, msg: { 
+  en: "---"
+  es: "Bájame que la prueba el Tato"
+}).upsert
+Excuse.new(_id: 79, msg: { 
+  en: "---"
+  es: "Bájame que estamos dando la nota"
+}).upsert
+Excuse.new(_id: 80, msg: { 
+  en: "---"
+  es: "Lo mio son las vías de 2 movimientos tipo lance"
+}).upsert
+Excuse.new(_id: 81, msg: { 
+  en: "---"
+  es: "Esta escuela es una caca"
+}).upsert
+Excuse.new(_id: 82, msg: { 
+  en: "---"
+  es: "Es que llevo días sin escalar"
+}).upsert
+Excuse.new(_id: 83, msg: { 
+  en: "---"
+  es: "No tengo feeling"
+}).upsert
+Excuse.new(_id: 84, msg: { 
+  en: "---"
+  es: "Si me sacan fotos voy fatal"
+}).upsert
+Excuse.new(_id: 85, msg: { 
+  en: "---"
+  es: "Con prisa no encadeno"
+}).upsert
+Excuse.new(_id: 86, msg: { 
+  en: "---"
+  es: "No tengo yemas"
+}).upsert
+Excuse.new(_id: 87, msg: { 
+  en: "---"
+  es: "Yo después tengo que conducir"
+}).upsert
+Excuse.new(_id: 88, msg: { 
+  en: "---"
+  es: "Déjamela en top que hoy voy de trankis"
+}).upsert
+Excuse.new(_id: 89, msg: { 
+  en: "---"
+  es: "Esta la hice hace mucho tiempo pero ahora no recuerdo como iba..."
+}).upsert
+Excuse.new(_id: 90, msg: { 
+  en: "---"
+  es: "Yo voy bien cuando veo la reunión"
+}).upsert
+Excuse.new(_id: 91, msg: { 
+  en: "---"
+  es: "Me lo cuentas cuando tengas mi edad"
+}).upsert
+Excuse.new(_id: 92, msg: { 
+  en: "---"
+  es: "Yo es que no se volar"
+}).upsert
+Excuse.new(_id: 93, msg: { 
+  en: "---"
+  es: "Con los niños no puedes competir"
+}).upsert
+Excuse.new(_id: 94, msg: { 
+  en: "---"
+  es: "He perdido pila"
+}).upsert
+Excuse.new(_id: 95, msg: { 
+  en: "---"
+  es: "No me gusta sudar"
+}).upsert
+Excuse.new(_id: 96, msg: { 
+  en: "---"
+  es: "Estos gatos me hacen daño"
+}).upsert
+Excuse.new(_id: 97, msg: { 
+  en: "---"
+  es: "Estoy cansado"
+}).upsert
+Excuse.new(_id: 98, msg: { 
+  en: "---"
+  es: "Ando justo de fuerzas"
+}).upsert
+Excuse.new(_id: 99, msg: { 
+  en: "---"
+  es: "Este arnés pesa mucho"
+}).upsert
+Excuse.new(_id: 100, msg: { 
+  en: "---"
+  es: "Antes la fiesta me sentaba mejor"
+}).upsert
+Excuse.new(_id: 101, msg: { 
+  en: "---"
+  es: "Es que los cantos están escondidos"
+}).upsert
+Excuse.new(_id: 102, msg: { 
+  en: "---"
+  es: "Esta vía no es de mi talla"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Es que me impresiona"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Es que no sabéis animar"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Hoy no es el día"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Hoy no me veo"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Es que hoy no se que me pasa"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Tengo las unas muy largas y me da miedo arrancarme una con esa regleta"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Estas vias tan cortas son un truno!"
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Es que a mi la continuidad me aburre..."
+}).upsert
+Excuse.new(_id: 103, msg: { 
+  en: "---"
+  es: "Tu has visto esta placa? Si me caigo aqui me dejare todos lo dientes!!!"
+}).upsert
